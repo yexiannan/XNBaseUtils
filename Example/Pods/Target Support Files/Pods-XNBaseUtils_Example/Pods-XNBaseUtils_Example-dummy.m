66cf3a3d40eb6df19bd0b932f43c2150d4f99da0
Luigi <932056276@qq.com>
2019-12-06 09:56:01 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-XNBaseUtils_Example/Pods-XNBaseUtils_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XNBaseUtils_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XNBaseUtils_Example
+@end
---
+++ Example/XNBaseUtils/XNViewController.m
@@ -0,0 +1,48 @@
+//
+//  XNViewController.m
+//  XNBaseUtils
+//
+//  Created by yexiannan on 12/05/2019.
+//  Copyright (c) 2019 yexiannan. All rights reserved.
+//
+
+#import "XNViewController.h"
+
+@interface XNViewController ()
+@property (nonatomic, strong) UILabel *label;
+@end
+
+@implementation XNViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.label = [UILabel new];
+    self.label.backgroundColor = [UIColor orangeColor];
+    self.label.textColor = [UIColor whiteColor];
+    self.label.text = @"asdghajsdfs";
+    self.label.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:18];
+//    self.label.numberOfLines = 0;
+    CGSize size = [self getSizeWithString:self.label.text withFont:self.label.font LimitWidth:150];
+    self.label.frame = CGRectMake(100, 100, size.width, size.height);
+    
+    
+    [self.view addSubview:self.label];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (CGSize)getSizeWithString:(NSString *)aStr withFont:(UIFont *)font LimitWidth:(CGFloat)aLimitWidth{
+    NSMutableParagraphStyle* paragraphStyle = [[NSMutableParagraphStyle alloc]init];
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraphStyle.alignment = NSTextAlignmentLeft;
+    NSDictionary *attributes = @{NSFontAttributeName:font, NSParagraphStyleAttributeName:paragraphStyle};
+    CGSize stringSize = [aStr boundingRectWithSize:CGSizeMake(aLimitWidth, MAXFLOAT) options:NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil].size;
+    return stringSize;
+}
+@end
